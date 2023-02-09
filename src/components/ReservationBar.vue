247183d0c8f64de813513ed692103a32e0b8e3d9
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2023-02-09 19:53:45 +0100

    Created a reservation bar component

---
+++ src/components/ReservationBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="reservation-block" :style="{width: totalWidth}"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+    hourWidth: number;
+    startHour: number;
+    endHour: number;
+}>();
+
+const reservationLength = computed(() => {
+    return props.endHour - props.startHour;
+});
+
+const totalWidth = computed(() => {
+    return reservationLength.value * props.hourWidth;
+});
+</script>
+
+<style scoped lang="scss">
+.reservation-block {
+    height: 100%;
+    background-color: #f00;
+}
+</style>
